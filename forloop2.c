26191aeb75df45c1c43db2e971fcb8745735054d
Xuhui Shi <146591740+sablju@users.noreply.github.com>
2023-10-13 10:09:37 +0000

    week3

---
+++ forloop2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+ int main() {
+    int a;
+    for (a = 1; a < 20; a+=2)
+    {
+        printf("a is equal to %d\n", a);
+    }
+    printf("I've finished this loop and a is equal to %d\n",a);
+
+    return (0);
+ }
