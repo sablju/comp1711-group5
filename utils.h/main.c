1a55476e0795b5422f4a97156e99063a4c982771
Xuhui Shi <146591740+sablju@users.noreply.github.com>
2023-10-23 16:00:26 +0000

    header file

---
+++ utils.h/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main () {
+    char filename [] = "data.txt";
+    FILE *file = open_file(filename, "w");
+    fclose(file);return 0;
+}
---
+++ utils.h/utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <stdlib.h>
+#include <stdio.h>
+
+FILE *open_file(char filename[], char mode[]);
+#endif /* UTILS_H */
